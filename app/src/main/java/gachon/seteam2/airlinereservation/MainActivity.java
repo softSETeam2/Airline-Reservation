03450f6b04fa3caa35c83d12302ea03da95d11bd
Limsehyuk <ish0430@naver.com>
2021-05-24 03:37:58 +0900

    drawer and logout

--- app/src/main/java/gachon/seteam2/airlinereservation/MainActivity.java
+++ app/src/main/java/gachon/seteam2/airlinereservation/MainActivity.java
@@ -2,20 +2,30 @@ package gachon.seteam2.airlinereservation;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,9 +36,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    String uid; // 사용자 로그인 정보
+    // 메인 액티비티
+    private Context mContext = MainActivity.this;
+    // 사용자 로그인 정보
+    public String uid = null;
+    public String name;
+    // 데이터베이스 정보
     Button searchBtn;
     private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    // 드로어, 네비게이션 뷰
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    private long backBtnTime = 0;
     int dy=0, dm=0, dd=0;
     int ay=0, am=0, ad=0;
     String start;
@@ -47,36 +69,24 @@ public class MainActivity extends AppCompatActivity {
         //액션 바 등록하기
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("메인화면");
+        //        actionBar.setDisplayHomeAsUpEnabled(true); //뒤로가기버튼
+//        actionBar.setDisplayShowHomeEnabled(true); //홈 아이콘
 
+        // 드로어, 네비게이션 뷰 등록
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerView = (View) findViewById(R.id.navigationView);
+        drawerLayout.setDrawerListener(listener); // 드로어 리스너 실행
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(navListener); // 네비게이션아이템 리스터 실행
+
+        //로그인 성공시
+        Toast.makeText(this, "환영합니다.", Toast.LENGTH_SHORT).show();
         //데이터베이스 연동
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        {
-            //사용자 로그인 정보 받기
-            Intent intent = getIntent();
-            uid = intent.getExtras().getString("uid");
-
-            mDatabase.child("Users").child(uid).addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    // Get Post object and use the values to update the UI
-                    if (dataSnapshot.getValue(User.class) != null) {
-                        User post = dataSnapshot.getValue(User.class);
-                        Log.w("FireBaseData", "getData" + post.toString());
-                    } else {
-                        Toast.makeText(MainActivity.this, "데이터 없음...", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    // Getting Post failed, log a message
-                    Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
-                }
-            });
-
-            Toast.makeText(this, "환영합니다.", Toast.LENGTH_SHORT).show();
-        }
-
+        //로그인된 사용자 정보 가져오기
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
         searchBtn=findViewById(R.id.searchBtn);
         searchBtn.setOnClickListener(new View.OnClickListener(){
@@ -159,22 +169,104 @@ public class MainActivity extends AppCompatActivity {
     // 액션바 등록
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar, menu) ;
-
-        return true ;
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
     }
+    //액션아이템 클릭 리스너
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.drawer :
-//                Toast.makeText(this, "drawer", Toast.LENGTH_SHORT).show();
-//                Intent drawerIntent = new Intent(getApplicationContext(), DrawerActivity.class);
-//                startActivity(drawerIntent);
-//                overridePendingTransition(R.anim.slide_enter,R.anim.none);
-                return true ;
+        switch (item.getItemId()) { // 우측상단 메뉴버튼 클릭시
+            case R.id.drawer:
+                // 사용자 정보 가져오기
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                // 이메일 set
+                TextView userEmail = findViewById(R.id.userEmail);
+                userEmail.setText(currentUser.getEmail());
+                drawerLayout.openDrawer(drawerView);
 
-            default :
-                return super.onOptionsItemSelected(item) ;
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    //드로어 레이아웃 클릭 이벤트 리스너 정의
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+        }
+    };
+
+    //네비게이션 드로어 아이템 클릭 이벤트 리스터 정의
+    NavigationView.OnNavigationItemSelectedListener navListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.testitem: // 아이템1: 테스트 빈공간
+                    Toast.makeText(getApplicationContext(), "SelectedItem 1", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.setting: // 아이템2: 설정화면으로 이동
+                    Intent settingIntent = new Intent(getApplicationContext(), DrawerActivity.class);
+                    startActivity(settingIntent);
+                    overridePendingTransition(R.anim.slide_enter, R.anim.none);
+                    break;
+                case R.id.logout: //아이템3: 로그아웃 버튼
+                    new AlertDialog.Builder(mContext) // 확인창 표시
+                            .setTitle("로그아웃").setMessage("로그아웃 하시겠습니까?")
+                            .setPositiveButton("로그아웃", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int whichButton) {
+                                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
+                                }
+                            }).setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+
+                        }
+                    })
+                            .show();
+                    return true;
+            }
+
+            DrawerLayout drawer = findViewById(R.id.drawerLayout);
+            drawer.closeDrawer(Gravity.RIGHT);
+            return true;
+        }
+    };
+
+    //뒤로가기 버튼 클릭시
+    @Override
+    public void onBackPressed() {
+        // 드로어가 열려있으면 뒤로가기 버튼 클릭시 드로어만 종료
+        DrawerLayout drawer = findViewById(R.id.drawerLayout);
+        if (drawer.isDrawerOpen(Gravity.RIGHT)) {
+            drawer.closeDrawer(Gravity.RIGHT);
+        } else {
+            long curTime = System.currentTimeMillis();
+            long gapTime = curTime - backBtnTime;
+            // 뒤로가기 두번 클릭시 앱 완전히 종료
+            if(0 <= gapTime && 2000 >= gapTime) {
+                ActivityCompat.finishAffinity(this);
+                System.exit(0);
+            }
+            else {
+                backBtnTime = curTime;
+                Toast.makeText(this, "한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
     private void getData() {
